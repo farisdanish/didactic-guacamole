6af9e5f0c65aa61e9d479ea4ef68579c7cfeff5c
farisdanish <farisdanxsh@gmail.com>
2024-03-26 14:05:23 +0800

    learned where to put css

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,10 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'eJKDB - Template Page')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/style.css') }}" />
   </head>
 <body>
     <header>
         <!--Navigation Bar-->
-        @yield('header-content')
+        <nav>
+            <ul>
+                <li><a href="#">eJKDB</a></li>
+                <li>Qairull Ashraff</li>
+            </ul>
+        </nav>
     </header>
     <main>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,14 +2,8 @@
 
 @section('title','eJKDB - Borang Permohonan')
 
-@section('header-content')
-@endsection
-
 @section('main-content')
 <h1>Borang Permohonan</h1>
-@endsection
-
-@section('footer-content')
 @endsection
 
 @section('scripts')
