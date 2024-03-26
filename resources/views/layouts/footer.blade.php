99bfa8a6506803ff42b8ac8e3b702966c572c460
farisdanish <farisdanxsh@gmail.com>
2024-03-26 15:28:51 +0800

    responsive nav bar completed

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,6 +4,18 @@
         @yield('footer-content')
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        function showSidebar(){
+          const sidebar = document.querySelector('.sidebar')
+          sidebar.style.display = 'flex'
+        }
+    </script>
+    <script>
+        function hideSidebar(){
+          const sidebar = document.querySelector('.sidebar')
+          sidebar.style.display = 'none'
+        }
+    </script>
     @yield('scripts')
 </body>
 </html>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,9 +12,14 @@
     <header>
         <!--Navigation Bar-->
         <nav>
+            <ul class="sidebar">
+                <li onclick="hideSidebar()"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
+                <li><a href="#">Qairull Ashraff</a></li>
+            </ul>
             <ul>
-                <li><a href="#">eJKDB</a></li>
-                <li>Qairull Ashraff</li>
+                <li><a href="#"><b>eJKDB</b></a></li>
+                <li class="hideOnMobile"><a href="#">Qairull Ashraff</a></li>
+                <li class="menu-button" onclick="showSidebar()"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 -960 960 960" width="28"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
             </ul>
         </nav>
     </header>
