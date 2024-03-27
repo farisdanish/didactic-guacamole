2034ac60ec2ed906d875b6a4ce71cf7d59b40ef2
farisdanish <farisdanxsh@gmail.com>
2024-03-27 09:13:17 +0800

    added form and dashed line

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,7 +4,9 @@
         <center>
         <span><img src="{{ asset('img/logojpkn.png') }}" width="90" height="60" alt="JPKN logo"><b>Hak Cipta Terpelihara Jabatan Perkhidmatan Komputer Negeri &copy; 2024</b></span>
         </center>
-      </footer>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         function showSidebar(){
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,18 @@
 @section('title','eJKDB - Borang Permohonan')
 
 @section('main-content')
-<div class="container">
-    <h1 class="applicationFormHeading">Borang Permohonan</h1>
+<div class="container applicationFormHeading">
+    <h1>Borang Permohonan</h1>
 </div>
-<div class="container borang">
+<div class="container-fluid borang_ejkdb">
     <form id="borang_ejkdb" action="" method="post">
         @csrf
+        <!-- Circles which indicates the steps of the form: -->
+        <div style="text-align:center;margin-top:40px;">
+            <span class="step">BAHAGIAN A,B,C</span>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path style="fill: gray" d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z"/></svg>
+            <span class="step">BAHAGIAN D,E,F</span>
+        </div>
     </form>
 </div>
 
