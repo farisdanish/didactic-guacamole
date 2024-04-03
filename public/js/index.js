2b6d1cc00c914127eededfe735ee0044167e5c13
farisdanish <farisdanxsh@gmail.com>
2024-04-03 13:41:34 +0800

    made some changes

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,7 @@ function showTab(n) {
     // ... and fix the Previous/Next buttons:
     if (n == 0) {
         document.getElementById("prevBtn").style.display = "none";
+        document.getElementById("nextBtn").setAttribute("type","button");
     } else {
         document.getElementById("prevBtn").style.display = "inline";
     }
@@ -26,26 +27,20 @@ function showTab(n) {
 function nextPrev(n) {
     // This function will figure out which tab to display
     var x = document.getElementsByClassName("tab");
-    
+    // Exit the function if any field in the current tab is invalid:
+    if (n == 1 && !validateForm()) return false;
+    // Hide the current tab:
+    x[currentTab].style.display = "none";
+    // Increase or decrease the current tab by 1:
+    currentTab = currentTab + n;
     // if you have reached the end of the form... :
-    if (currentTab == (x.length - 1)) {
+    if (currentTab >= x.length) {
         //...the form gets submitted:
-        document.getElementById("nextBtn").setAttribute("type","submit");
-        document.getElementById("borang-ejkdb").submit();
+        document.getElementById("borang_ejkdb").submit();
         return false;
-    }else{
-        // Exit the function if any field in the current tab is invalid:
-        //if (n == 1 && !validateForm()) return false;
-        // Hide the current tab:
-        x[currentTab].style.display = "none";
-        //scroll to top of page on next/prev button click
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-        // Increase or decrease the current tab by 1:
-        currentTab = currentTab + n;
-        //display the correct tab:
-        showTab(currentTab);
     }
+    // Otherwise, display the correct tab:
+    showTab(currentTab);
 }
 
 function validateForm() {
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -20,7 +20,7 @@
             <ul>
                 <li><a href="#" style="font-size: 22px;"><b>eJKDB</b></a></li>
                 <!--<li class="hideOnMobile"><a href="#">Qairull Ashraff</a></li>-->
-                <li class="hideOnMobile"><a href="#"><b>@yield('username', 'Faris Danish')</b></a><>
+                <li class="hideOnMobile"><a href="#"><b>@yield('username', 'Faris Danish')</b></a></li>
                 <li class="menu-button" onclick="showSidebar()"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 -960 960 960" width="28"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
             </ul>
         </nav>
