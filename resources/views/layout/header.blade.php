f2d994abfea1b08b73b6cf02c5cfe4451644ab30
farisdanish <farisdanxsh@gmail.com>
2024-04-15 11:51:31 +0800

    first commit since raya holiday, minor changes

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
   <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
-    <a class="navbar-brand brand-logo" href="{{ url('/') }}">
-      <img src="{{ url('assets/images/logo.svg') }}" alt="logo" /> </a>
-    <a class="navbar-brand brand-logo-mini" href="{{ url('/') }}">
-      <img src="{{ url('assets/images/logo-mini.svg') }}" alt="logo" /> </a>
+    <a class="navbar-brand brand-logo" href="{{ url('/') }}" style="font-size: 22px;">
+      <b>eJKDB</b> </a>
+    <a class="navbar-brand brand-logo-mini" href="{{ url('/') }}" style="font-size: 15px;">
+      <b>eJKDB</b> </a>
   </div>
   <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
     <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
@@ -11,7 +11,7 @@
     </button>
     <ul class="navbar-nav navbar-nav-left header-links">
       <li class="nav-item d-none d-xl-flex">
-        <a href="#" class="nav-link">Schedule <span class="badge badge-primary ml-1">New</span>
+        <a href="#" class="nav-link">Schedule</a><span class="badge badge-primary ml-1">New</span>
         </a>
       </li>
       <li class="nav-item active d-none d-lg-flex">
@@ -115,7 +115,7 @@
       </li>
       <li class="nav-item dropdown d-none d-xl-inline-block">
         <a class="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
-          <span class="profile-text d-none d-md-inline-flex">Richard V.Welsh !</span>
+          <span class="profile-text d-none d-md-inline-flex">Faris Danish</span>
           <img class="img-xs rounded-circle" src="{{ url('assets/images/faces/face8.jpg') }}" alt="Profile image"> </a>
         <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
           <a class="dropdown-item p-0">
