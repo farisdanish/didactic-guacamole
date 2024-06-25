65a84a5bdeec7f6b8c2d99966b22a06aab43d591
farisdanish <farisdanxsh@gmail.com>
2024-06-25 16:27:27 +0800

    login changes again

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    // Specify the custom table name
+    protected $table = 'pengguna';
+
     /**
      * The attributes that are mass assignable.
      *
